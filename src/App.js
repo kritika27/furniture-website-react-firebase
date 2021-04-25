e2547ee8a1fc25b52f4339e5623373e62fbb1f26
kritika <kritikasri27@gmail.com>
2021-04-25 21:28:57 +0530

    Added routes for different pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import React from "react";
-
+//import About from "./components/About/About";
+//import Furniture from "./components/Furniture";
+import Home from "./components/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
+import ProductList from "./components/Products/ProductList";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import About from "./components/About/About";
 export default function App() {
-  return <div>App</div>;
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home}></Route>
+          <Route exact path="/products" component={ProductList}></Route>
+          <Route exact path="/about" component={About}></Route>
+        </Switch>
+      </Router>
+    </>
+  );
 }
