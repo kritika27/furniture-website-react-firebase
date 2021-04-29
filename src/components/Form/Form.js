4f6a2452f0a878926088e2cf984b2dd191a6d7ee
kritika <kritikasri27@gmail.com>
2021-04-29 22:10:06 +0530

    Added blog section

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,49 +3,31 @@ import React from "react";
 export default function Form() {
   return (
     <div>
-      <section id="contact" class="contact flex-columns">
-        <div class="row">
-          <div class="column">
-            <div class="column-1">
+      <section id="contact" className="contact flex-columns">
+        <div className="row">
+          <div className="column">
+            <div className="column-1">
               <img
-                //src=""
-                src="../../images/kam-idris-nylcMEgK8EQ-unsplash.jpg"
-                alt=""
+                src="https://raw.githubusercontent.com/kritika27/portfolio/main/img/thought-catalog-505eectW54k-unsplash.jpg"
+                alt="blog"
               />
             </div>
           </div>
-          <div class="column">
-            <div class="column-2 bg-light">
-              <h2>Request Callback</h2>
+          <div className="column">
+            <div className="column-2 bg-light">
+              <h2>Blog Posts</h2>
               <form action="" class="callback-form">
-                <div class="form-control">
-                  <label for="name"></label>
-                  <input
-                    type="text"
-                    name="name"
-                    id="name"
-                    placeholder="Enter name"
-                  />
+                <div className="form-control">
+                  <p>
+                    {" "}
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Totam quaerat, modi doloremque necessitatibus eum dolor
+                    nesciunt delectus, voluptate blanditiis, obcaecati beatae ab
+                    aut ipsa consequuntur
+                  </p>
                 </div>
-                <div class="form-control">
-                  <label for="email"></label>
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    placeholder="Enter email"
-                  />
-                </div>
-                <div class="form-control">
-                  <label for="phone"></label>
-                  <input
-                    type="text"
-                    name="phone"
-                    id="phone"
-                    placeholder="Enter phone"
-                  />
-                </div>
-                <input type="submit" value="Send" id="submit" class="btn" />
+
+                <button>Read More</button>
               </form>
             </div>
           </div>
