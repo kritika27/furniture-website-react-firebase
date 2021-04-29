e748c23b97769644e94807d849c1daf2884aad26
kritika <kritikasri27@gmail.com>
2021-04-30 00:29:44 +0530

    Removed unwanted code

--- src/utils/Auth.js
+++ src/utils/Auth.js
@@ -1,12 +1,11 @@
 import { auth } from "./firebase";
+
 export default function Auth() {
   const provider = new auth.GoogleAuthProvider();
 
   return auth()
     .signInWithPopup(provider)
     .then((result) => {
-      //var credential = result.credential;
-      //var token = credential.accessToken;
-      //var user = result.user;
+      console.log(result.user);
     });
 }
