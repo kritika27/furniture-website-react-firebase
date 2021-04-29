768c9ed4700a31c1b83d7a297be6a56164b43c59
kritika <kritikasri27@gmail.com>
2021-04-29 22:05:08 +0530

    Added navbar component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import { auth } from "../../utils/firebase";
+import { useHistory } from "react-router-dom";
+import Auth from "../../utils/Auth";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const history = useHistory();
+  const [firstName, setFirstName] = useState("");
+
+  const googleSignIn = async () => {
+    try {
+      await Auth();
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    auth().onAuthStateChanged(function (user) {
+      if (user) {
+        setFirstName(user.displayName);
+        console.log(firstName);
+        history.push("/products");
+      } else console.log("no user");
+    });
+  }, [history, firstName]);
+
   return (
     <header>
       <nav id="nav">
@@ -20,6 +44,23 @@ const Navbar = () => {
             </li>
             <li>
               <Link to="/products">Products</Link>
+            </li>
+            <li>
+              {firstName ? (
+                <button>
+                  <Link
+                    to="/"
+                    onClick={() => {
+                      auth().signOut();
+                      setFirstName("");
+                    }}
+                  >
+                    Sign Out
+                  </Link>
+                </button>
+              ) : (
+                <button onClick={googleSignIn}>Sign Up</button>
+              )}
             </li>
           </ul>
         </div>
