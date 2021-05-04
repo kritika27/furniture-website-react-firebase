e0cfaf493920f13df92113809d91244f6a875c3d
kritika <kritikasri27@gmail.com>
2021-05-05 00:16:58 +0530

    Add path of routes in a single file

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import About from "./About/About";
+import Error from "./Error/Error";
+import Home from "./Home/Home";
+import ProductList from "./Products/ProductList";
+
+export { About, Error, Home, ProductList };
